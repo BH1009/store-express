c11dd838cef94f31ad32707fe1a001bdd06415db
Brayan Hernandez <hbrayan148@gmail.com>
2023-10-05 13:58:00 -0600

    add button functions

---
+++ src/public/main.js
@@ -0,0 +1,10 @@
+const deleteButton = document.querySelector('button.delete')
+const editButton = document.querySelector('button.edit')
+
+deleteButton.addEventListener("click",  () => {
+    console.log("clicked button delete")
+})
+
+editButton.addEventListener('click', () => {
+    console.log("clicked button edit")
+})
